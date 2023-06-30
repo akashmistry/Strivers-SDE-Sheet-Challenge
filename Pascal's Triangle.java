e15e43fc701f4499729ecd04bb1a1673a36da6ea
Akash <akash.mistry@innodeed.com>
2023-06-30 15:42:04 +0530

    Pascal's Triangle

--- Pascal's Triangle.java
+++ Pascal's Triangle.java
@@ -7,29 +7,22 @@ public class Solution {
 	public static ArrayList<ArrayList<Long>> printPascal(int n) {
         // Write your code here.
 
-		ArrayList<ArrayList<Long>> ans = new ArrayList<ArrayList<Long>>();
+		ArrayList<ArrayList<Long>> triangle = new ArrayList<ArrayList<Long>>();
 		
-		ArrayList<Long> temp = new ArrayList<>();
-		temp.add(1L);
-		ans.add(temp);
+		for(int i=1;i<=n;i++){
+			long ans =1;
+			ArrayList<Long> triangleRow = new ArrayList<>();
+			triangleRow.add(ans);
 
-		for(int i = 1; i<n; i++){
-
-			ArrayList<Long> prev = ans.get(i-1);
-			ArrayList<Long> curr = new ArrayList<>();
-
-			curr.add(1L);
-
-			for(int j = 2; j<=i; j++){
-				long a1 = prev.get(j-2);
-				long a2 = prev.get(j-1);
-				curr.add(a1+a2);
+			for(int j=1;j<i;j++){
+				ans = ans*(i-j);
+				ans = ans/j;
+				triangleRow.add(ans);
 			}
-			curr.add(1L);
-			ans.add(curr);
+			triangle.add(triangleRow);
 		}
 
-		return ans;
+		return triangle;
 		
 	}
 }
